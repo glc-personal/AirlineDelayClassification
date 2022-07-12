600cf05e72c2b109d2460c5b9938c243a0096145
glc-personal <108988066+glc-personal@users.noreply.github.com>
2022-07-11 23:48:07 -0400

    Update prepocess.py
    Made several changes. Modified preprocess() such that the data is read into a pandas DataFrame, split, the unique columns for one-hot encoding are found (in a parallel way), then the one-hot encoding occurs after the new columns are added to the DataFrame. Added a new internal function _get_new_column() which is used for the aforementioned step necessary for one-hot encoding. Lastly, a merge_dataframe function was written to merge DataFrames.

--- prepocess.py
+++ prepocess.py
@@ -15,15 +15,21 @@ def preprocess(csv_fname, n, pool):
     dfs = []
     dfs = _split_dataframe(df, n)
 
+    # Get the unique columns.
+    new_columns = pool.map(_get_new_columns, dfs) # Parallelize this and then gather all of it together to concat
+    new_columns = list(set(new_columns[0]))
+    df_categories = pd.DataFrame(index=np.arange(0, len(df)), columns=new_columns).fillna(0)
+    df = pd.concat([df, df_categories], axis=1, ignore_index=False, sort=False)
+
     # One-hot encode the original data.
-    #df_one_hot_encoded = _one_hot_encode(df)
-    
     dfs_one_hot_encoded = pool.map(_one_hot_encode, dfs)
-    print(len(dfs_one_hot_encoded))
+
+    # Merge the DataFrames back together.
+    df_one_hot_encoded = merge_dataframes(dfs_one_hot_encoded)
 
     # Convert to a PyTorch Tensor object.
-    #x = Tensor(df_one_hot_encoded.values)
-    #return x
+    x = Tensor(df_one_hot_encoded.values)
+    return x
 
 def _split_dataframe(df, n):
     dfs = []
@@ -38,17 +44,12 @@ def _split_dataframe(df, n):
         dfs.append(df.iloc[istart:iend,:])
     return dfs
 
-def _one_hot_encode(df):
-    '''
-    One-Hot Encodes a Dataframe object for given column names.
-    '''
+def _get_new_columns(df):
     arr_col_names = ['Airline', 'AirportFrom', 'AirportTo']
+    new_columns = []
 
     # Get the number of entries in the Dataframe.
     n_entries = len(df)
-
-    # Initialize the one-hot encoded Dataframe object.
-    df_one_hot_encoded = df
 
     # For each column determine the number of categories.
     for col_name in arr_col_names:
@@ -66,20 +67,51 @@ def _one_hot_encode(df):
                 appendage = '_TO'
             else:
                 appendage = ''
-        df_categories = pd.DataFrame(index=np.arange(0, n_entries), columns=[(category + appendage) for category in arr_unique_categories]).fillna(0)
+            new_columns.append(str(category) + appendage)
+    
+    return new_columns
+
+
+def _one_hot_encode(df):
+    '''
+    One-Hot Encodes a Dataframe object for given column names.
+    '''
+    arr_col_names = ['Airline', 'AirportFrom', 'AirportTo']
+
+    # Get the number of entries in the Dataframe.
+    n_entries = len(df)
+
+    # Initialize the one-hot encoded Dataframe object.
+    df_one_hot_encoded = df
+
+    # For each column determine the number of categories.
+    for col_name in arr_col_names:
+
+        if col_name == 'AirportFrom':
+            appendage = '_FROM'
+        elif col_name == 'AirportTo':
+            appendage = '_TO'
+        else:
+            appendage = ''
 
         # Fill the categories Dataframe according to the original Dataframe.
         i = 0
         for value in df[col_name]:
             # One-hot encode the categories Dataframe.
-            for category in df_categories:
-                if (value + appendage) == category:
-                    df_categories.loc[i, category] = 1.0
+            for category in df.columns:
+                if (str(value) + appendage) == category:
+                    df_one_hot_encoded.loc[i, category] = 1
             i = i + 1
 
         # Remove the column from the original data.
         del df_one_hot_encoded[col_name]
 
-        # Concat the one-hot encoded Dataframe objects to the original data.        
-        df_one_hot_encoded = pd.concat([df_one_hot_encoded, df_categories], axis=1, ignore_index=False, sort=False)
+    # Concat the one-hot encoded Dataframe objects to the original data.        
+    #df_one_hot_encoded = df_one_hot_encoded.astype(int)
+    #df_one_hot_encoded = pd.concat([df_one_hot_encoded, df_categories], axis=1, ignore_index=False, sort=False)
     return df_one_hot_encoded
+
+def merge_dataframes(dfs):
+    # Merge the DataFrames together.
+    df = pd.concat(dfs, ignore_index=True, sort=False)
+    return df
