a01cd82912a4e39a4889f43c0b254b1a0537658b
glc-personal <108988066+glc-personal@users.noreply.github.com>
2022-07-11 23:44:34 -0400

    Update AirlineDelayClassification_PyTorch_Kaggle.py
    Removed the use of short.csv to test the changes to preprocess.py which now utilizes the Pool object from multiprocessing more effectively to reduce computational time for the CPU heavy tasks. Performs the preprocessing (including one-hot encoding and dataset splitting) around 70 seconds for the full dataset.

--- AirlineDelayClassification_PyTorch_Kaggle.py
+++ AirlineDelayClassification_PyTorch_Kaggle.py
@@ -29,11 +29,10 @@ if __name__ == '__main__':
     ncores = mp.cpu_count()
 
     # Instantiate the airline Dataset.
-    #dataset = AirlineDataset('short.csv', cores=ncores, jobspercore=10)
     dataset = AirlineDataset('Airlines.csv', cores=ncores, jobspercore=10)
 
     # Split the data and load into DataLoader objects.
-    #train_dataloader, test_dataloader, val_dataloader = dataset.load(percentages=[80,10,10], shuffle=True)
+    train_dataloader, test_dataloader, val_dataloader = dataset.load(percentages=[80,10,10], shuffle=True)
 
     # Create a model for training with the train_datalaoder *********
 
